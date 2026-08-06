3ecd7663fd8c612b281ca6d3ad5653d11754c21a
runner <runner@hmpps-github-actions-runner-656b7d875f-m6qf8>
2026-08-06 10:42:14 +0000

    Rename project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-datahub-query-metrics"
--- src/main/kotlin/uk/gov/justice/digital/hmpps/datahubquerymetrics/DatahubQueryMetrics.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/datahubquerymetrics/DatahubQueryMetrics.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.datahubquerymetrics
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class DatahubQueryMetrics
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<DatahubQueryMetrics>(*args)
 }
